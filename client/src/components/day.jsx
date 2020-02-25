5320b4f4f7e31f9a64a0a29b8656c829668c47db
kk-mbugua <kamau.dev@gmail.com>
2020-02-25 14:57:18 -0800

    basic layout

---
+++ client/src/components/day.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Grid } from "@material-ui/core"
+class Day extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Grid container direction="column">
+                
+            </Grid>
+         );
+    }
+}
+ 
+export default Day;
---
+++ client/src/components/shift.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Typography} from "@material-ui/core"
+class Shift extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <Typography> Name </Typography>
+            </div>
+         );
+    }
+}
+ 
+export default Shift;
